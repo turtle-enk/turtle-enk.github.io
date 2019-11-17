9399c48f13058f7b05f14a23f819ec9b249da9cf
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-11-18 06:46:45 +0900

    Update tate.js

--- tate.js
+++ tate.js
@@ -1,71 +1,17 @@
-/*
-* CssSwitcher jQuery Plugin 0.0.2
-* Copyright (c) 2010-2011 thingsym (www.thingslabo.com)
-* $Date: 11.06.27 13:56:41 $
-* $Rev: 002 $
-* Dual licensed under the MIT and GPL licenses.
-* http://www.opensource.org/licenses/mit-license.php
-* http://www.gnu.org/licenses/gpl.html
-
-* required jQuery 1.4.+ (http://docs.jquery.com/Downloading_jQuery)
-* required Cookie Plugin (http://plugins.jquery.com/project/Cookie)
-*/
-
-var CssSwitcher = {
-  // Settings
-  cssPath : "", // http://domain/path/to/css/
-  idList : [ 'tate', 'yoko' ], // List up id attribute of 'stylesheet' rel attribute
-  // Cookie Settings
-  domain : "",
-  path : "/",
-  expires : 7, // days or a Date object Wdy, DD-Mon-YYYY HH:MM:SS GMT (ex. Fri, 01-Dec-2010 20:40:03 GMT)
-  secure : 0, // 0 or 1
-
-  change : function(json) {
-    for (var i=0; i<json.length; i++){
-      if (json[i].theme) {
-        $('#' + json[i].id).attr({href : this.cssPath + json[i].theme + ".css"});
-      }
-    }
-
-    var cookieVal = [];
-    for (var i=0; i<this.idList.length; i++) {
-      var match_id;
-      for (var j=0; j<json.length; j++) {
-        if (this.idList[i] == json[j].id) {
-          cookieVal.push(["{'id':'", json[j].id, "'\,'theme':'", json[j].theme, "'}"].join(''));
-          match_id = json[j].id;
-          break;
-        }
-      }
-
-      for (var j=0; j<this.json.length; j++) {
-          if (this.idList[i] == this.json[j].id && this.idList[i] != match_id) {
-            cookieVal.push(["{'id':'", this.json[j].id, "'\,'theme':'", this.json[j].theme, "'}"].join(''));
-          }
-      }
-    }
-
-    $.cookie('CssSwitcher', '['+cookieVal+']', { expires: this.expires, path: this.path, domain: this.domain, secure: this.secure });
-    this.getJSON();
-  },
-
-  getJSON : function() {
-    var value = $.cookie('CssSwitcher');
-    if (value) {
-      this.json = eval(value);
-    }
-    else {
-      this.json = eval([{}]);
-    }
-  },
-
-  init : function() {
-    window.onunload = function() {};
-    this.getJSON();
-    this.change(this.json);
-  }
-
+function setMyCookie() {
+  myCookieVal = $('p').hasClass('tate') ? 'isActive' : 'notActive';
+  $.cookie('myCookieName', myCookieVal, { expires: 365, path: '/' });
 }
-
-CssSwitcher.init();
+var flg = "off";
+$("#tate").click(function () {
+  $('p').toggleClass('tate');
+  $(this).toggleClass('is-active');
+  if(flg == "off"){
+    $(this).html("<input type="button" value="縦書き" onclick="changeStyle('');">");
+    flg = "on";
+  }else{
+    $(this).html("<input type="button" value="横書き" onclick="changeStyle('');">");
+    flg = "off";
+  }
+  setMyCookie();
+});
