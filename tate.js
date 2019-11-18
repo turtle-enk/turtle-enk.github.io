f324da0f26ed79fe874692a69adfb82e92a32ecd
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-11-18 17:34:53 +0900

    Update tate.js

--- tate.js
+++ tate.js
@@ -1,17 +1,5 @@
-function setMyCookie() {
-  myCookieVal = $('p').hasClass('tate') ? 'isActive' : 'notActive';
-  $.cookie('myCookieName', myCookieVal, { expires: 365, path: '/' });
-}
-var flg = "off";
-$("#js-switch-mode").click(function () {
-  $('p').toggleClass('tate');
-  $(this).toggleClass('is-active');
-  if(flg == "off"){
-    $(this).html("<i class='fas fa-moon'></i>");
-    flg = "on";
-  }else{
-    $(this).html("<i class='fas fa-moon'></i>");
-    flg = "off";
-  }
-  setMyCookie();
+function(){
+  $(#switch).click(function(){
+    $(".honbun").toggleClass("tate");
+});
 });
