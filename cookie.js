857e64e8bd853df9198c3be57663874471e46982
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-11-21 01:45:21 +0900

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,19 @@
+<script>
+var event = $(#tate).get(0).onclick;
+
+$(function(){
+   if($.cookie("tate")){
+      $("#tate").get(0).onclick = event;
+  } else {
+  $("#tate").get(0).onclick = "";
+  
+$("#tate").click(function() {
+      $.cookie("tate" , "tate" , { expires: 30, path: "/" });
+      }
+$("#yoko").click(function() {
+      $.removeCookie("tate" , { path: "/" });
+    }
+  return false;
+  });
+});
+</script>
