5645c2b73fd8b568d119b1c72941bd726df0c4b0
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-11-24 05:00:26 +0900

    Update windowheight.js

--- windowheight.js
+++ windowheight.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   hsize = $(window).height();
-  $("main").css("height", hsize + "px");
+  $("body").css("height", hsize + "px");
 });
 $(window).resize(function () {
   hsize = $(window).height();
-  $("main").css("height", hsize + "px");
+  $("body").css("height", hsize + "px");
 });
