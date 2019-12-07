0bd020c71c8b3515dddf10384b1e8eada2b89c3f
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-12-07 15:16:58 +0900

    Update tatecookie.js

--- tatecookie.js
+++ tatecookie.js
@@ -3,7 +3,7 @@ $(function() {
   $('#button1').click(function() {
  
 //クッキー登録
-$.cookie('cookie1', 'tate1', {expire:7});
+$.cookie('cookie1', 'tate1', {expires:7});
 
     // 所定の要素のテキストを書き換える
   $('#tatecss').html('<link rel="stylesheet" href="//turtle-enk.github.io/tategaki.css" type="text/css" id="tatelink">');
