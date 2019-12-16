cac4e61760ffe07f9cf1cfd095d743e97e5ea29f
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-12-17 01:30:19 +0900

    Create width.js

---
+++ width.js
@@ -0,0 +1,13 @@
+$windowWidth = window.screen.width; // 画面の横幅
+$breakPoint = 600;
+ 
+isMobileSize = ($windowWidth <= $breakPoint);
+isPcSize = ($windowWidth > $breakPoint);
+
+if(isMobileSize){
+//モバイルサイズの場合の記述
+}
+
+if(isPcSize){
+//PCサイズの場合の記述
+}
