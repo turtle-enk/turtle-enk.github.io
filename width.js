2f0300b63236b4cdb615daaab0a56f5250b4333b
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2019-12-17 01:34:36 +0900

    Update width.js

--- width.js
+++ width.js
@@ -6,8 +6,8 @@ isPcSize = ($windowWidth > $breakPoint);
 
 if(isMobileSize){
 //モバイルサイズの場合の記述
-}
+$("hmain").css("max-width", 100 + "%");}
 
 if(isPcSize){
 //PCサイズの場合の記述
-}
+$("hmain").css("max-width", 600 + "px");}
