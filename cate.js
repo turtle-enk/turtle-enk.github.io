d43e30e288f4408342e414fe5c931ab75dcf7d1e
turtle-enk <57871458+turtle-enk@users.noreply.github.com>
2020-06-08 22:52:42 +0900

    Create cate.js

---
+++ cate.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	var linkID = "mix";	// リンクを設定するタグのID
+	var linkURL = "/category.php?cid=1";
+	var baseTag = document.getElementById(linkID);
+	var link1 = baseTag.firstChild.nodeValue;
+	var aTag = document.createElement("a");
+	aTag.href = linkURL;
+	aTag.appendChild(document.createTextNode(link1));
+	baseTag.replaceChild(aTag, baseTag.firstChild);
+}
